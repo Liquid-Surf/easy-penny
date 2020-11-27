da2b1a5a70b49fe4bc1e29c453a26dcf3c6ed3db
Vincent <git@vincenttunru.com>
2020-11-27 15:24:22 +0100

    Add hook to fetch and update data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@ import * as React from "react";
 import Head from "next/head";
 import { Layout, } from "../components/Layout";
 import { SessionGate } from "../components/SessionGate";
-import { useSessionInfo } from "../hooks/sessionInfo";
+import { useProfile } from "../hooks/profile";
+import { getStringNoLocale } from "@inrupt/solid-client";
+import { foaf } from "rdf-namespaces";
 
 const Home: React.FC = () => {
-  const info = useSessionInfo();
+  const profile = useProfile();
+
+  const name = profile ? getStringNoLocale(profile.data, foaf.name) : "you";
 
   return (
     <Layout>
@@ -14,7 +18,7 @@ const Home: React.FC = () => {
       </Head>
       <div className="md:w-4/5 lg:w-1/2 mx-auto pt-5 md:pt-20">
         <SessionGate>
-          Welcome {info?.webId}!
+          Welcome {name}!
         </SessionGate>
       </div>
     </Layout>

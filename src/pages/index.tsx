87bc34afb03580a5bb6fc5b0e5c1cb189fbd1efa
Vincent <git@vincenttunru.com>
2021-01-17 10:55:10 +0100

    Browse storages on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,19 @@ import Head from "next/head";
 import { Layout } from "../components/Layout";
 import { SessionGate } from "../components/SessionGate";
 import { useProfile } from "../hooks/profile";
-import { asUrl, getStringNoLocale } from "@inrupt/solid-client";
-import { foaf } from "rdf-namespaces";
+import { asUrl, getStringNoLocale, getUrlAll } from "@inrupt/solid-client";
+import { foaf, space, vcard } from "rdf-namespaces";
 import Link from "next/link";
 
 const Home: React.FC = () => {
   const profile = useProfile();
 
-  const name = profile ? getStringNoLocale(profile.data, foaf.name) : "you";
+  const name = profile
+    ? getStringNoLocale(profile.data, foaf.name) ?? getStringNoLocale(profile.data, vcard.fn)
+    : "you";
   const webId = profile ? asUrl(profile.data) : "";
+
+  const storages = profile ? getUrlAll(profile.data, space.storage) : [];
 
   return (
     <Layout>
@@ -20,10 +24,18 @@ const Home: React.FC = () => {
       </Head>
       <div className="md:w-4/5 lg:w-1/2 mx-auto pt-5 md:pt-20">
         <SessionGate>
-          Welcome {name}!
           <Link href={`/explore/${encodeURIComponent(webId)}`}>
-            <a>Profile</a>
+            <a className="py-5 block hover:text-coolGray-700 focus:underline focus:text-coolGray-700 focus:outline-none">{name}</a>
           </Link>
+          <ul className="space-between-5">
+            {storages.map(storageUrl => (
+              <li key={storageUrl}>
+                <Link href={`/explore/${encodeURIComponent(storageUrl)}`}>
+                  <a className="p-5 bg-coolGray-700 hover:bg-coolGray-900 rounded text-white block focus:ring-2 focus:ring-offset-2 focus:ring-coolGray-700 focus:outline-none focus:ring-opacity-50">{storageUrl}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </SessionGate>
       </div>
     </Layout>

5632f28ffc47e46add831b0a9c4c04460956ef8b
Vincent <git@vincenttunru.com>
2021-01-19 20:08:29 +0100

    Remove duplicate predicates in the <datalist>

--- src/components/data/NamespaceDatalist.tsx
+++ src/components/data/NamespaceDatalist.tsx
@@ -1,15 +1,16 @@
+import { UrlString } from "@inrupt/solid-client";
 import * as knownPredicates from "rdf-namespaces";
 import { FC } from "react";
 
 interface Props {
   id: string;
 };
+const allKnownPredicates = Array.from(new Set(Object.values(knownPredicates).map(namespace => Object.values(namespace)).flat()));
 
 export const NamespaceDatalist: FC<Props> = (props) => {
-  const allKnownPredicates = Object.values(knownPredicates).map(namespace => Object.values(namespace)).flat();
   return (
     <datalist id={props.id}>
-      {allKnownPredicates.map(predicate => <option key={predicate as unknown as string} value={predicate as unknown as string}/>)}
+      {allKnownPredicates.map(predicate => <option key={predicate} value={predicate}/>)}
     </datalist>
   );
 };

ec94f32e2866613de7177964a287eaa2d1203595
joe <you@example.com>
2022-08-18 00:17:50 +0200

    Show card on webID url

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import { useState } from "react";
 import Head from "next/head";
 import { isContainer, UrlString } from "@inrupt/solid-client";
 import { Layout } from "./Layout";
 import { useResource } from "../hooks/resource";
 import { DatasetViewer } from "./viewers/DatasetViewer";
+import { Card } from "./viewers/Card";
 import { ContainerViewer } from "./viewers/ContainerViewer";
 import { FetchErrorViewer } from "./viewers/FetchErrorViewer";
 import { FileViewer } from "./viewers/FileViewer";
@@ -17,24 +19,44 @@ interface Props {
 }
 
 export const Explorer: React.FC<Props> = (props) => {
-  const resource = useResource(typeof props.url === "string" ? props.url : null);
+  const [show, setShow] = useState(false);
+
+  const resource = useResource(
+    typeof props.url === "string" ? props.url : null
+  );
   const sessionInfo = useSessionInfo();
 
-  const datasetViewer = resource !== null && isLoadedDataset(resource)
-    ? <DatasetViewer dataset={resource}/>
-    : null;
+  const datasetViewer =
+    resource !== null && isLoadedDataset(resource) ? (
+      <DatasetViewer dataset={resource} />
+    ) : null;
 
-  const containerViewer = resource !== null && isLoadedDataset(resource) && isContainer(resource.data)
-    ? <ContainerViewer dataset={resource}/>
-    : null;
+  const containerViewer =
+    resource !== null &&
+    isLoadedDataset(resource) &&
+    isContainer(resource.data) ? (
+      <ContainerViewer dataset={resource} />
+    ) : null;
 
-  const fileViewer = resource !== null && isLoadedFileData(resource)
-    ? <FileViewer file={resource}/>
-    : null;
+  const fileViewer =
+    resource !== null && isLoadedFileData(resource) ? (
+      <FileViewer file={resource} />
+    ) : null;
 
-  const errorViewer = typeof sessionInfo === "undefined" || resource === null || (!isLoadedDataset(resource) && resource.isValidating)
-    ? <Spinner/>
-    : <FetchErrorViewer error={resource.error}/>;
+  const errorViewer =
+    typeof sessionInfo === "undefined" ||
+    resource === null ||
+    (!isLoadedDataset(resource) && resource.isValidating) ? (
+      <Spinner />
+    ) : (
+      <FetchErrorViewer error={resource.error} />
+    );
+
+  const card =
+    window.location.href.slice(-4) == "card" ||
+    window.location.href.slice(-4) == "d#me" ? (
+      <Card />
+    ) : null;
 
   return (
     <Layout path={props.url}>
@@ -44,7 +66,11 @@ export const Explorer: React.FC<Props> = (props) => {
       <div className="lg:w-4/5 xl:w-2/3 2xl:w-1/2 mx-auto p-5 md:pt-20">
         {errorViewer}
         {containerViewer}
-        {fileViewer || datasetViewer}
+        {card}
+        <button onClick={() => setShow(!show)}>
+          {show ? "Hide" : "Show"} advanced options
+        </button>
+        {fileViewer || show ? datasetViewer : null}
       </div>
     </Layout>
   );
---
+++ src/components/viewers/Card.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+interface CardProps {
+  title: string;
+}
+
+export const Card: FC<CardProps> = (props) => {
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  );
+};

7f58314394ff0173ccdad751b898450c91118d08
joeitu <tmey@itu.dk>
2023-02-10 12:26:25 +0100

    better build script

--- build_serverui.sh
+++ build_serverui.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 src_svg_foler="src_svg_$(date +%d-%m-%y_%H-%M-%S)"
-trash src-build
-cp -r src src-build
+trash /tmp/src-build
+cp -r src /tmp/src-build
 # trash src
 mv src $src_svg_foler
-rm src-build/pages/index.tsx src-build/pages/explore.tsx src-build/pages/turtle.tsx
-mv src-build/pages/server_ui.tsx src-build/pages/\[\[...slug\]\].tsx
-ln -s $PWD/src-build $PWD/src
+rm /tmp/src-build/pages/index.tsx /tmp/src-build/pages/explore.tsx /tmp/src-build/pages/turtle.tsx
+mv /tmp/src-build/pages/server_ui.tsx /tmp/src-build/pages/\[\[...slug\]\].tsx
+mv /tmp/src-build $PWD/src
 NEXT_PUBLIC_MODE="integrate" npx next build &&
 NEXT_PUBLIC_MODE="integrate" npx next export -o server-ui &&
 mv 'server-ui/[[...slug]]/index.html' server-ui/index.html

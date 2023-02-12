a907ed78f683109570e6acce7364b30e4869f239
joeitu <tmey@itu.dk>
2023-02-12 22:23:28 +0100

    fix type error

--- src/functions/explorer.ts
+++ src/functions/explorer.ts
@@ -1,10 +1,11 @@
 // These functions are useful for putting Penny in "integrate" mode,
 // i.e. preinstalled on a Pod as its native interface.
 
+import { LoadedCachedDataset } from "../hooks/dataset";
 
 	// are we in the the solid server's root directory ?
 	// if so, we much use "house" icon instead of "folder"
-export function isRootContainter(resource): Boolean {
+export function isRootContainter(resource: LoadedCachedDataset): Boolean {
   const url = new URL(resource.data.internal_resourceInfo.sourceIri)
   const nb_slashes = url.pathname.split('/').length -1
   return nb_slashes <= 1 

22dfb7014ec508dd43e9778f582b2540f8710783
Vincent <git@vincenttunru.com>
2023-03-21 07:44:26 +0100

    Add scripts to build locally

---
+++ scripts/build_server_ui.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env zx
+import "zx/globals";
+
+await fs.move("src/pages/index.tsx", "scripts/.cache/src/pages/index.tsx");
+await fs.move("src/pages/explore.tsx", "scripts/.cache/src/pages/explore.tsx");
+await fs.move("src/pages/turtle.tsx", "scripts/.cache/src/pages/turtle.tsx");
+await fs.move("src/pages/server_ui.tsx", "src/pages/[[...slug]].tsx");
+
+let exitCode = 0;
+let error = "";
+
+try {
+  await $`NEXT_PUBLIC_MODE="integrate" npm run build`;
+
+  await fs.move("out", "server-ui");
+  await fs.move("server-ui/\[\[...slug\]\]/index.html", "server-ui/index.html");
+} catch (processOutput) {
+  /** @type {ProcessOutput} */
+  const output = processOutput;
+  exitCode = output.exitCode;
+  error = output.error;
+}
+
+await fs.move("scripts/.cache/src/pages/index.tsx", "src/pages/index.tsx");
+await fs.move("scripts/.cache/src/pages/explore.tsx", "src/pages/explore.tsx");
+await fs.move("scripts/.cache/src/pages/turtle.tsx", "src/pages/turtle.tsx");
+await fs.move("src/pages/[[...slug]].tsx", "src/pages/server_ui.tsx");
+
+if (exitCode !== 0) {
+  console.error(error);
+  process.exit(exitCode);
+}
---
+++ scripts/build_standalone.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env zx
+import "zx/globals";
+
+await fs.move("src/pages/server_ui.tsx", "scripts/.cache/src/pages/server_ui.tsx");
+
+let exitCode = 0;
+let error = "";
+
+try {
+  await $`npm run build`;
+} catch (processOutput) {
+  /** @type {ProcessOutput} */
+  const output = processOutput;
+  exitCode = output.exitCode;
+  error = output.error;
+}
+
+await fs.move("scripts/.cache/src/pages/server_ui.tsx", "src/pages/server_ui.tsx");
+
+if (exitCode !== 0) {
+  console.error(error);
+  process.exit(exitCode);
+}

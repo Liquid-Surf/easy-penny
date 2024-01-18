a9d1a502e41de52177bf9121c0a15a18514673c8
Vincent <git@vincenttunru.com>
2024-01-18 21:31:43 +0100

    Re-enable linting on precommit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-  // Temporarily disabled due to https://github.com/vercel/next.js/issues/30145
-  // "*.{ts,tsx,js,jsx}": (filenames) =>
-  //   `next lint --fix --file ${filenames
-  //     .map((file) => file.split(process.cwd())[1])
-  //     .join(' --file ')}`,
+  "*.{ts,tsx,js,jsx}": (filenames) =>
+    `next lint --fix --file ${filenames
+      .map((file) => file.split(process.cwd())[1])
+      .join(" --file ")}`,
   "*.{ts,tsx,js,jsx,css,md}": "prettier --write",
 };
